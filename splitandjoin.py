708e28b1d7de7eb9e6e062e78bbcddaa6b356b62
Roopa-kr <nisharoopa3@gmail.com>
2026-02-13 20:28:29 +0530

    String split and join problem

---
+++ splitandjoin.py
@@ -0,0 +1,8 @@
+def split_join(line):
+    splitted=line.split()
+    joined="-".join(splitted)
+    return joined
+if __name__=="__main__":
+    line=input("enter the characters")
+    result=split_join(line)
+    print(result)
