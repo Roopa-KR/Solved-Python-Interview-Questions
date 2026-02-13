16240c3df496e5367b2792d5c47916e850ea8a46
Roopa-kr <nisharoopa3@gmail.com>
2026-02-13 20:30:35 +0530

    first and last name problem

---
+++ firstandlastname.py
@@ -0,0 +1,9 @@
+def print_full_name(first, last):
+    # Write your code here
+    return f"Hello {first} {last}! You just deleved into python"
+
+
+if __name__ == '__main__':
+    first_name = input()
+    last_name = input()
+    print(print_full_name(first_name, last_name))
