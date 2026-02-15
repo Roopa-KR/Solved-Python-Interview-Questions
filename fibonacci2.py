64a0225e488bb2231ce5c4062fd7edd8bcbeec27
Roopa-kr <nisharoopa3@gmail.com>
2026-02-15 14:44:30 +0530

    Fibonacci using function

---
+++ fibonacci2.py
@@ -0,0 +1,12 @@
+def fibonacci(n):
+    series = []
+    a, b = 0, 1
+    
+    for i in range(n):
+        series.append(a)
+        a, b = b, a + b
+        
+    return series
+
+n = int(input("Enter number of terms: "))
+print(fibonacci(n))
