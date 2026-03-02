35f49cdbe3e673d56889e858f7b137a8a63a6c1b
Roopa-kr <nisharoopa3@gmail.com>
2026-03-02 12:30:26 +0530

    Know how to access object

---
+++ obj_access.py
@@ -0,0 +1,10 @@
+#Access the properties of an object: using dot notation|
+class Car:
+  def __init__(self, brand, model):
+    self.brand = brand
+    self.model = model
+
+car1 = Car("Toyota", "Corolla")
+
+print(car1.brand)
+print(car1.model)
