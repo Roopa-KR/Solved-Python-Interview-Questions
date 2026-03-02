dfd9f93c62e4e0ff5bf5cffab098228dfd87adf3
Roopa-kr <nisharoopa3@gmail.com>
2026-03-02 12:37:36 +0530

    method calling using self

---
+++ method_call.py
@@ -0,0 +1,14 @@
+#calling methods with self:
+class Person:
+  def __init__(self, name):
+    self.name = name
+
+  def greet(self):
+    return "Hello, " + self.name
+
+  def welcome(self):
+    message = self.greet()
+    print(message + "! Welcome to our website.")
+
+p1 = Person("Tobias")
+p1.welcome()
