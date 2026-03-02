ce6a2293b3518d3afab995c5a522f90b273f2ded
Roopa-kr <nisharoopa3@gmail.com>
2026-03-02 22:30:37 +0530

    factorial sum of each digit

--- strong_number.py
+++ strong_number.py
@@ -1,4 +1,8 @@
+# A strong number is a number whose sum of the factorial of its digits is equal to the original number.
+# For example, 145 is a strong number because 1! + 4! + 5! = 145.
+# Write a program to check if a given number is a strong number or not.   
 s=list(map(int, input("try 145 :")))
+
 l=[]
 def fact(n):
     if n==0 :
