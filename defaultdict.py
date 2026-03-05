c1d81427cbf9ec5d2dd675ab0b9c7e85697b9ec3
Roopa-kr <nisharoopa3@gmail.com>
2026-03-05 10:55:36 +0530

    Printing index if string is present in defaultdict

---
+++ defaultdict.py
@@ -0,0 +1,14 @@
+
+from collections import defaultdict
+d=defaultdict(list)
+n,m=map(int,input().split())
+for i in range(1,n+1):
+    word=input().strip()
+    d[word].append(i)
+words=[]
+for w in range(m):
+    words.append(input().strip())
+for word in words:
+    if word in d:
+        print(*d[word])
+    
