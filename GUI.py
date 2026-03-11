7ee00a3c0c397d566cfa51c49d9a13ea65fa8c09
Roopa-kr <nisharoopa3@gmail.com>
2026-03-11 17:10:13 +0530

    Introduction to GUI

---
+++ GUI.py
@@ -0,0 +1,6 @@
+#Graphical user interface for the program
+from tkinter import *
+# tkinter is a built-in library in Python for creating graphical user interfaces (GUIs).
+# It provides a set of tools and widgets to create windows, buttons, labels, and other GUI elements.
+# GUI is used to create interactive applications that can be used by users to perform various tasks.
+# In this code, we will create a simple GUI application using tkinter.
