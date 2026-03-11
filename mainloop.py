12509b61b71170c01cf700221d942b444d339820
Roopa-kr <nisharoopa3@gmail.com>
2026-03-11 18:25:24 +0530

    Running of main loop

---
+++ mainloop.py
@@ -0,0 +1,10 @@
+#10. mainloop() – Run Application
+#Description: Runs the event loop and keeps the window open.
+
+from tkinter import *
+
+root = Tk()
+
+Label(root, text="Window Running").pack()
+
+root.mainloop()
